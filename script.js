49bca23581bf3fd0768d219d095ebfb79b346ae0
Jarod Gowgiel <jarod.gowgiel@tufts.edu>
2017-11-16 17:15:19 -0500

    Code required to query google maps API for local pizza shops and place them on the map.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 function loadMap() {
+
     /* Fetch the user location */
     navigator.geolocation.getCurrentPosition(function (position) { 
         var curr_lat = position.coords.latitude;
         var curr_lng = position.coords.longitude;
-
+        var map_lat_lng = new google.maps.LatLng(curr_lat, curr_lng);
         /* Instantiate and initialize the map at the current location */
         map = new google.maps.Map(document.getElementById('map'), {
             zoom: 15,
@@ -14,7 +15,7 @@ function loadMap() {
 
         /* Place a marker to ID the user onto the map */
         var user = new google.maps.Marker({
-                position: new google.maps.LatLng(curr_lat, curr_lng),
+                position: map_lat_lng,
                 zIndex: 100
             });
 
@@ -28,6 +29,56 @@ function loadMap() {
         });
         user.setMap(map);
 
+        /* Search within one mile */
+        var restaurant_req = {
+            location: map_lat_lng,
+            radius: 1600,
+            query: 'pizza delivery'
 
-    } );
+        };
+
+        /* Retrieves all nearby pizza shops */
+        var nearby = new google.maps.places.PlacesService(map);
+        var infowindow;
+        var results_cache;
+        nearby.textSearch(restaurant_req, function(results, status) {
+            
+            /* Place on the map */
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    var pizza_loc = results[i].geometry.location;
+
+                    /* Only place locations within one mile */
+                    var distance = google.maps.geometry.spherical.computeDistanceBetween(pizza_loc, map_lat_lng);
+                    if (distance < 1600) {
+
+                        /* Create the new marker */
+                        var pizza_mrk = new google.maps.Marker({
+                            position: results[i].geometry.location,
+                            title: results[i].name
+                        });
+
+                        /* Place it on the map */
+                        pizza_mrk.setMap(map);
+
+                        /* Add a listener to open the infowindow */
+                        pizza_mrk.addListener("click", function() {
+                            
+                            infowindow = new google.maps.InfoWindow({
+                                content: "<p>" + this.title + "</p>",
+                            });
+
+                            infowindow.open(map, this);
+                        });
+
+
+                    }
+
+                }
+            }
+        });
+
+    });
+
+
 }
