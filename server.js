31aba35c1ebc6786c4c61d53a3ec0b5901e29d1a
Martin Gao <martinigao88@yahoo.com>
2017-12-08 12:17:21 -0500

    basic server to serve homepage

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
 
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', function(req, res){
 	res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
 
-
+http.listen(process.env.PORT || 3000, function() {
+  	console.log('share-pizza is running on port 3000');
+});
