5d3ad06c497392ff2ebba821fa13f6ad3ccf8830
Angela <angelabsun@yahoo.com>
2017-12-09 18:05:00 -0500

    added modal

--- node-js-archetype/pages/script.js
+++ node-js-archetype/pages/script.js
@@ -132,12 +132,35 @@ function addRequests()
             distance_from = google.maps.geometry.spherical.computeDistanceBetween(me, this.position)/1609.344;
             contentString = '<p class="login">'+this.pizza_type+'<p/><p>is <span class="distance"> ' + distance_from.toString() + "</span> miles away<p/>\
             <button id="myBtn">Add to request</button><div id="myModal" class="modal">\
-  <div class="modal-content">\
-    <span class="close">&times;</span>\
-    <p>Some text in the Modal..</p>\
-  </div>\
-</div>";
+              <div class="modal-content">\
+                <span class="close">&times;</span>\
+                <p>Some text in the Modal..</p>\
+              </div>\
+            </div>";
+        var modal = document.getElementById('myModal');
 
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
             infowindow = new google.maps.InfoWindow({
                 content: contentString
             });
