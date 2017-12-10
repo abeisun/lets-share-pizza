d4a84c51907ab215c259bf2f05d0e9591b9851e6
Martin Gao <martinigao88@yahoo.com>
2017-12-10 18:34:13 -0500

    backtrack

--- server.js
+++ server.js
@@ -64,8 +64,7 @@ app.get('/allOrders.json', function(req, res) {
 
 app.post('/addToOrder', function(req, res) {
     orderModel.findByIdAndUpdate(req.body.objID, 
-        { $push: { 'contacts': req.body.contact },
-        $inc: { 'numSlices': req.body.numSlices } }, 
+        { $inc: { 'numSlices': req.body.numSlices } }, 
         function (err, updatedOrder) {
             if (err) {
                 console.log(err);
