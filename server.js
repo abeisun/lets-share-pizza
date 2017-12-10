8fa72d1d6c8e2d7c0be7e977f740361aa3c8116b
Martin Gao <martinigao88@yahoo.com>
2017-12-10 18:31:12 -0500

    fix typo

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/allOrders.json', function(req, res) {
 app.post('/addToOrder', function(req, res) {
     orderModel.findByIdAndUpdate(req.body.objID, 
         { $push: { 'contacts': req.body.contact },
-        $inc: { 'size': req.body.numSlices } }, 
+        $inc: { 'numSlices': req.body.numSlices } }, 
         function (err, updatedOrder) {
             if (err) {
                 console.log(err);
