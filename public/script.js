2536572a837f20df9529d49614afe1d8193077fb
LCameron <lindacameron17@gmail.com>
2017-12-10 18:14:13 -0500

    pizza original req

--- public/script.js
+++ public/script.js
@@ -1,10 +1,13 @@
-
+var pizzashop_name = "pizzaShop";
+var curr_lat;
+var curr_lng;
+var self = this;
 function loadMap() {
 
     /* Fetch the user location */
     navigator.geolocation.getCurrentPosition(function (position) { 
-        var curr_lat = position.coords.latitude;
-        var curr_lng = position.coords.longitude;
+        curr_lat = position.coords.latitude;
+        curr_lng = position.coords.longitude;
         var map_lat_lng = new google.maps.LatLng(curr_lat, curr_lng);
         /* Instantiate and initialize the map at the current location */
         map = new google.maps.Map(document.getElementById('map'), {
@@ -23,20 +26,20 @@ function loadMap() {
         user.addListener("click", function() {
                 infowindow = new google.maps.InfoWindow({
                     content: 
-    '<h2>New Pizza Request</h2>\
-    <div class="submitButton">\
-        <form action="/startOrder" method="post">\
-            How many slices would you like?<br>\
-            <input id="slices" type="text" name="num_slices"><br>\
-            How should other people get in touch with you?<br>\
-            <input id="contact" type="text" placeholder="Email, phone, etc." name="contact_info"><br>\
-            What kind of pizza do you want?<br>\
-            <input id="pizza_type" type="text" name="type_of"><br>\
-            What pizza shop are you ordering from?<br>\
-            <input id="pizza_shop" type="text" name="shop"><br>\
-            <input type="submit" value="Submit"></button>\
-        </form>\
-    </div>'
+                    '<h2>New Pizza Request</h2>\
+                    <div class="submitButton">\
+                        <form action="/startOrder" method="post">\
+                            How many slices would you like?<br>\
+                            <input id="slices" type="text" name="num_slices"><br>\
+                            How should other people get in touch with you?<br>\
+                            <input id="contact" type="text" placeholder="Email, phone, etc." name="contact_info"><br>\
+                            What kind of pizza do you want?<br>\
+                            <input id="pizza_type" type="text" name="type_of"><br>\
+                            What pizza shop are you ordering from?<br>\
+                            <input id="pizza_shop" type="text" name="shop"><br>\
+                            <input type="submit" value="Submit"></button>\
+                        </form>\
+                    </div>'
 
                 });
                 infowindow.open(map, user);
@@ -69,37 +72,75 @@ function loadMap() {
                         /* Create the new marker */
                         var pizza_mrk = new google.maps.Marker({
                             position: results[i].geometry.location,
-                            title: results[i].name
+                            pizza_shop: results[i].name
                         });
 
                         /* Place it on the map */
                         pizza_mrk.setMap(map);
 
+//                        pizzashop_name = results[i].name;
+
                         /* Add a listener to open the infowindow */
                         pizza_mrk.addListener("click", function() {
                             
                             infowindow = new google.maps.InfoWindow({
-                                content: "<p>" + this.title + "</p>",
-                            });
+                                content: //this.title + 
+                                 this.pizza_shop +
 
+                                /* modal */
+                                '<div class="req_body">\
+                                    <button class="buttons" type="button" data-toggle="modal" data-target="#initReqButton">Create Pizza Request</button>\
+                                        <div class="modal fade right" id="initReqButton" role="dialog">\
+                                        <div class="modal-content">\
+                                            <span class="close">&times;</span>\
+                                            <div class="modal-header">\
+                                                <h4 class="modal-title">Pizza Request Form</h4>\
+                                            </div>\
+                                            <div class="modal-body">\
+                                                <form id = "neworder_form">\
+                                                    Name: <input type = "text" name = "input" value = ""/></br></br>\
+                                                    Number of Slices: <input type = "text" name = "input" value = ""/></br></br>\
+                                                    Toppings: <input type = "text" name = "input" value = ""/></br></br>\
+                                                    Phone Number: <input type = "text" name = "input" value = ""/></br></br>\
+                                                    Pizza Shop Name: <input type = "text" name = "input" value = ""/></br></br>\
+                                                </form>\
+                                                <button type = "button" onclick="startOrder()" class= "btn btn-default" role="button">Submit\
+                                            </div>\
+                                        </div>\
+                                        </div>\
+                                </div>'
+                                            /*<div class = "modal-footer">\
+                                                <button type = "button" class="btn btn-default" data-dismiss="modal">Submit</button>\
+                                            </div>\*/
+                                        //</div>'
+                            });
                             infowindow.open(map, this);
                         });
-
-
                     }
-
                 }
             }
         });
-
     });
     getCurrentRequests();
 }
 
+function startOrder()
+{
+    var form = document.getElementById("neworder_form");
+    //var name = form.elements[0].value;
+    var numSlices = form.elements[1].value;
+    var toppings = form.elements[2].value;
+    var contact = { 'name': form.elements[0].value, 'phoneNumber': form.elements[3].value };
+    var pizzaShopName = form.elements[4].value; //self.pizzashop_name //this.title;
+    url = "https://lets-share-pizza.herokuapp.com/startOrder";
+    $.post(url, {'numSlices': numSlices, 'pizzaShopName': pizzaShopName, 'toppings': toppings, 'contactInfo': [ contact ], 'coordinates': [curr_lat, curr_lng] });
+}
+
+
 function getCurrentRequests()
 {
     request = new XMLHttpRequest();
-    request.open("POST", "https://lets-share-pizza.herokuapp.com/addToOrder", true);
+    request.open("POST", "https://lets-share-pizza.herokuapp.com/requestSlice", true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.onreadystatechange = function() {//Call a function when the state changes.
     if(request.readyState == 4 && request.status == 200) {
@@ -110,7 +151,7 @@ function getCurrentRequests()
 }
 function parseData()
 {
-    orders = JSON.parse(request.responseText);
+    locations = JSON.parse(request.responseText);
     addRequests();
 }
 
@@ -121,50 +162,51 @@ function addRequests()
         scaledSize: new google.maps.Size(50, 50)
     };
     //var image = "cat_icon.png";
-    for (var order in orders){
+    for (var location in order){
         marker = new google.maps.Marker({
-            position: new google.maps.LatLng(order.coordinates[0], order.coordinates[1]),
+            position: new google.maps.LatLng(location.lat, locations.lng),
             map:map,
             icon: image,
+            login: locations.people[person].login
         });
 
-    google.maps.event.addListener(marker, 'click', function (){
-        distance_from = google.maps.geometry.spherical.computeDistanceBetween(me, this.position)/1609.344;
-        contentString = '<p class="login">'+this.toppings+'<p/><p>is <span class="distance"> ' + distance_from.toString() + '</span> miles away<p/>\
-        <button id="myBtn">Add to request</button><div id="myModal" class="modal">\
-          <div class="modal-content">\
-            <span class="close">&times;</span>\
-            <p>Some text in the Modal..</p>\
-          </div>\
-        </div>';
-    var modal = document.getElementById('myModal');
+        google.maps.event.addListener(marker, 'click', function (){
+            distance_from = google.maps.geometry.spherical.computeDistanceBetween(me, this.position)/1609.344;
+            contentString = '<p class="login">'+this.toppings+'<p/><p>is <span class="distance"> ' + distance_from.toString() + '</span> miles away<p/>\
+            <button id="myBtn">Add to request</button><div id="myModal" class="modal">\
+              <div class="modal-content">\
+                <span class="close">&times;</span>\
+                <p>Some text in the Modal..</p>\
+              </div>\
+            </div>';
+        var modal = document.getElementById('myModal');
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
             modal.style.display = "none";
         }
-    }
-        infowindow = new google.maps.InfoWindow({
-            content: contentString
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+            infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            infowindow.open(map, this);
         });
-        infowindow.open(map, this);
-    });
     }
 }
