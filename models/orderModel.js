93e0641e6741836ed501e18878dfda345b2235da
Martin Gao <martinigao88@yahoo.com>
2017-12-10 12:38:11 -0500

    schema and models created

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const order = new Schema({
-    numSlices: int,
-    pizzaShop: String,
-    topping: String,
-    contacts: [ { name: String, phoneNumber: String } ],
-    createdAt: { type: Date, expires: 3600 },
+const orderSchema = new Schema({
+    numSlices: { type: Number, min: 0, max: 8 },
+    pizzaShopName: String,
+    toppings: String,
+    // contactInfo: [{ name: String, phoneNumber: String }],
+    coordinates: [{ type: Number }],
+    createdAt: { type: Date, default: Date.now, expires: 7200 }
 }
+
+module.exports = mongoose.model('orders', orderSchema);
--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-
 const app = express();
 const http = require('http').Server(app);
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));   //serve css and javascript along side html pages
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -17,18 +16,46 @@ app.listen(app.get('port'), function() {
 });
 
 const mongoose = require('mongoose');
-var mongoDB = 'mongodb://127.0.0.1/pizza';
+var mongoDB = 'mongodb://127.0.0.1/pizza';    //transition to mlab eventually
 
 mongoose.connect(mongoDB, {
     useMongoClient: true
 });
 
+var orderModel = require('../models/orderModel.js');
+
 app.get('/', function(req, res){
 	res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
 
-app.post('/newRequest', function(req, res){
-    var numSlices = req.body.
+app.post('/startOrder', function(req, res){
+    var order = new orderModel({
+        numSlices: req.body.numSlices,
+        pizzaShopName: req.body.pizzaShopName,
+        toppings: req.body.toppings,
+        // contactInfo: req.body.contactInfo,
+        coordinates: req.body.coordinates
+    });
+
+    order.save(function (err, order) {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                message: "Error when creating order",
+                error: err
+            });
+        }
+        return res.status(201).json(order);
+    });
+});
+
+
+
+
+
+    var numSlices = parseFloat(req.body.numSlices);
+    var pizzaShop = req.body.pizzaShop;
+    var toppings = req.body.
     res.send();
 });
 
